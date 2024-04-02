d24748d632d0a2bf9d74b479599bf20705dc7326
JoaoGabrielTN <joaogt008@gmail.com>
2024-04-02 10:20:51 -0300

    csv and pickle

--- data.py
+++ data.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 import pandas as pd
+import requests
+import pickle 
 import os
 
 CONSUMER_KEY = os.environ.get('ALPHA_KEY')
-url = f'jjjapikey={CONSUMER_KEY}'
-print(url)
+url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey={CONSUMER_KEY}'
+r = requests.get(url)
+data = pd.DataFrame.from_dict(r.json(), orient='index')
+print(data)
+data.to_csv("df.csv", index=False)
+with open("data_pickle.pkl", "wb") as f:
+    pickle.dump(data, f)
