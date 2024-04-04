1f1dc1c0c78f544f4c74c730684dd189bc824484
JoaoGabrielTN <joaogt008@gmail.com>
2024-04-04 20:49:54 -0300

    jupyter notebook para visualização de dados

--- scripts/data.py
+++ scripts/data.py
@@ -7,8 +7,12 @@ import os
 CONSUMER_KEY = os.environ.get('ALPHA_KEY')
 url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey={CONSUMER_KEY}'
 r = requests.get(url)
+print(type(r))
+to_pikle = r.json()
+type(to_pikle)
 data = pd.DataFrame.from_dict(r.json(), orient='index')
 print(data)
 data.to_csv("df.csv", index=False)
-with open("data_pickle.pkl", "wb") as f:
-    pickle.dump(data, f)
+with open("pickle.pkl", "wb") as f:
+    pickle.dump(to_pikle, f)
+
